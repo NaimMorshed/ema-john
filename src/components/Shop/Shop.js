c95e1b233cea0e60aea923d31d7df79d146b807b
Naim Morshed <morshed.naim13@gmail.com>
2021-02-27 23:50:11 +0600

    second commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './Shop.css';    
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const productHandle = (product) => {
+        console.log("Product added: ", product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    products.map(
+                        product => 
+                            <Product 
+                                productHandle = {productHandle}
+                                product={product}>
+                            </Product>
+                    )
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
