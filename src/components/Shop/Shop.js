2c82ef17d003cabf64d2dbb39825fb165e642a86
Naim Morshed <morshed.naim13@gmail.com>
2021-03-06 11:23:31 +0600

    ninth

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,19 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';    
 import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
 
+    useEffect(() => {
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const previousCart = productKeys.map(keys => {
+            const product = fakeData.find(pd => pd.key === keys);
+            product.quantity = savedCart[keys];
+            return product;
+        })
+        setCart(previousCart);
+    }, []);
+
     const productHandle = (product) => {
-        const newCart = [...cart, product];
-        setCart(newCart);
-        const count = newCart.filter(cart => cart.key === product.key);
+        const toBeAddedKey = product.key;
+        const sameProduct = cart.find(cart => cart.key === toBeAddedKey);
+        let count = 1;
+        let newCount;
+        if (sameProduct) {
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key === product.key)
+            newCount = [...others, sameProduct];
+        } else {
+            product.quantity = 1;
+            newCount = [...cart, product];
+        }
+        setCart(newCount);
+        
         addToDatabaseCart(product.key, count.length);
     }
 
